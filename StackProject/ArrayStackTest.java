fb66ce6151519b844566cbeff9f6a042318c4d0a
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-03-06 22:55:08 -0800

    Finished JUNIT for 3 main methods in LinkedStack

--- StackProject/ArrayStackTest.java
+++ StackProject/ArrayStackTest.java
@@ -4,7 +4,7 @@ public class ArrayStackTest
 {
     public static void main(String[] args) throws Exception {
         ResizableArrayStack<String> stack = new ResizableArrayStack<>();
-        String[] bag2 = {"B", "C", "C", "D", "E", "B"};
+        String[] stack1 = {"B", "C", "C", "D", "E", "B"};
         String example = "a*b/(c-a)+d*e";
         String postfix = "2 3 * 4 2 - / 5 6 * + ";
         System.out.println(stack.convertToPostfix(example));
