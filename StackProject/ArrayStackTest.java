d8925484efb1190fe688239021a5cc7cd5236b2e
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-03-06 23:01:05 -0800

    JUNIT test pop in  ArrayStackTest

--- StackProject/ArrayStackTest.java
+++ StackProject/ArrayStackTest.java
@@ -1,13 +1,30 @@
 package StackProject;
+import org.junit. *;
+import static org.junit.Assert. *;
 
 public class ArrayStackTest
 {
-    public static void main(String[] args) throws Exception {
-        ResizableArrayStack<String> stack = new ResizableArrayStack<>();
-        String[] stack1 = {"B", "C", "C", "D", "E", "B"};
-        String example = "a*b/(c-a)+d*e";
-        String postfix = "2 3 * 4 2 - / 5 6 * + ";
-        System.out.println(stack.convertToPostfix(example));
-        System.out.println(stack.evaluatePostfix(postfix));
+    StackInterface<String> s1 = new ResizableArrayStack<String>();
+    {   s1.push("c");
+        s1.push("-");
+        s1.push("a");
+        s1.push("*");
+        s1.push("(");
+        s1.push("n");
+        s1.push("-");
+        s1.push("b");
+        s1.push(")");
+        s1.push("*");
+        s1.push("f");
+        s1.push("+");
+        s1.push("z");
     }
+
+    @Test
+         
+        public void testpop() throws Exception
+        {
+            
+            assertEquals("z",(s1.pop()));
+        }
 }
